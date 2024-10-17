791db39d0641950b2200767ad17e9e2e94631135
rohitbisht01 <rb9519208@gmail.com>
2024-10-17 21:27:19 +0530

    feat: deploying application

--- backend/index.js
+++ backend/index.js
@@ -3,18 +3,32 @@ const blogRoute = require("./routes/blog");
 const userRoute = require("./routes/user");
 const connectDb = require("./db/connectDb");
 const cookieParser = require("cookie-parser");
+const path = require("path");
+const cors = require("cors");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 4000;
+
+// const __dirname = path.resolve();
 
 const app = express();
 connectDb();
 
 app.use(cookieParser());
 app.use(express.json());
+app.use(cors());
 
 app.use("/api/v1/blog", blogRoute);
 app.use("/api/v1/user", userRoute);
+
+// Serve frontend files in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`server running on ${PORT}`);
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,21 @@
-import path from "path";
+import path, { dirname } from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:4000",
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
